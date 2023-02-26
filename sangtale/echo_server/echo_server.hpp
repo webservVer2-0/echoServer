32556a855b23da4b70a84aad9c0b6b9dd85e6038
semo <a01088107455@gmail.com>
2023-02-26 14:48:44 +0900

    FEAT : 운영체제 확인

--- sangtale/echo_server/echo_server.hpp
+++ sangtale/echo_server/echo_server.hpp
@@ -4,13 +4,18 @@
 #include <fcntl.h>
 #include <sys/event.h>
 #include <sys/socket.h>
-#include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 #include <iostream>
 #include <map>
 #include <vector>
+
+#ifdef linux
+printf("linux compiled\n");
+#else
+#include <sys/time.h>
+#endif
 
 void exit_with_error(const std::string &msg);
 
